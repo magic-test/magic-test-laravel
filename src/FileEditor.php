370f65288c19ddcdaf50b74933bc8174ca2345cd
Mateus Guimarães <mateus.jatene@gmail.com>
2021-03-01 03:13:02 -0300

    it works!!

---
+++ src/FileEditor.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Mateusjatenee\MagicTest;
+
+use Illuminate\Support\Str;
+use Mateusjatenee\MagicTest\Grammar\Grammar;
+
+class FileEditor
+{
+    public function process(string $content, $grammar, string $method)
+    {
+
+        $arrayContent = explode("\n", $content);
+
+
+        $test = "\n";
+
+
+         $after = Str::of($content)
+            ->after($method)
+            ->after('$browser->')
+            ->before("});\n");
+
+        $after = explode("\n", $after)[0];
+
+
+        $started = false;
+        $newText = [];
+
+        foreach ($arrayContent as $key => $subContent) {
+            if (Str::contains(trim($subContent), trim($after))) {
+                $started = true;
+                $newText[] = Str::replaceLast(";", "", $subContent);
+                foreach ($grammar as $grammarKey => $g) {
+                    $isLast = ($grammarKey + 1) == $grammar->count();
+                    $test = $g->build($isLast);
+                    $newText[] = $test;
+                }
+
+
+
+                if (trim($arrayContent[$key + 1]) == "") {
+                    $started = false;
+                }
+
+            } else {
+                if ($started) {
+                    if (Str::endsWith(trim($subContent), ';')) {
+                        $started = false;
+                        continue;
+                    } else {
+                        continue;
+                    }
+                }
+                $newText[] = $subContent;
+            }
+        }
+
+
+        return implode("\n", $newText);
+
+        $matches = [];
+
+        $toReplace = (string) Str::of($content)
+            ->after($method)
+            ->after($after)
+            ->before(');') . ');';
+
+        // When it only has a visits call
+        if (
+            Str::endsWith(preg_replace('~(\v|\t|\s{2,})~m', '', $toReplace), '});')) {
+            $prependNewTest = "\n" . Grammar::indent('$browser');
+            $test = ";\n" . $prependNewTest . $test;
+            $toReplace = ');';
+        }
+
+
+
+
+        $newContent = Str::replaceFirst($toReplace, $test, $content);
+
+        return $newContent;
+    }
+}
---
+++ tests/FileEditorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Mateusjatenee\MagicTest\Tests;
+
+use Mateusjatenee\MagicTest\FileEditor;
+use Mateusjatenee\MagicTest\Grammar\Click;
+use Mateusjatenee\MagicTest\Grammar\See;
+use Mateusjatenee\MagicTest\MagicTest;
+use Mateusjatenee\MagicTest\MagicTestManager;
+
+class FileEditorTest extends TestCase
+{
+    /** @test */
+    public function it_properly_replaces_the_method_content_when_it_does_not_have_actions()
+    {
+        $expectedInput = file_get_contents(__DIR__ . '/fixtures/ExampleTest.example');
+        $expectedOutput = file_get_contents(__DIR__ . '/fixtures/ExampleTestOutput.example');
+        
+
+        $grammar = collect([
+            new Click('click', '', "'Log in'", [], [], 'a'),
+            new Click('click', '', "'Forgot your password?'", [], [], 'a'),
+            new See('see', '', "'Mateus'", [], [], 'span'),
+        ]);
+
+        $processedText = (new FileEditor)->process($expectedInput, $grammar, 'testBasicExample');
+
+        $this->assertEquals($expectedOutput, $processedText);
+    }
+
+    /** @test */
+    public function it_properly_replaces_the_content_when_it_has_actions()
+    {
+        $expectedInput = file_get_contents(__DIR__ . '/fixtures/ExampleTestWithContent.example');
+        $expectedOutput = file_get_contents(__DIR__ . '/fixtures/ExampleTestOutput.example');
+        
+
+        $grammar = collect([
+            new Click('click', '', "'Log in'", [], [], 'a'),
+            new Click('click', '', "'Forgot your password?'", [], [], 'a'),
+            new See('see', '', "'Mateus'", [], [], 'span'),
+        ]);
+
+        $processedText = (new FileEditor)->process($expectedInput, $grammar, 'testBasicExample');
+
+        $this->assertEquals($expectedOutput, $processedText);
+    }
+}
