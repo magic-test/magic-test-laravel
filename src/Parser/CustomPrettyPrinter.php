20eee65c03f0851bdf9b9a7c7fff376dbc3fc597
Mateus Guimarães <mateus.jatene@gmail.com>
2021-03-10 11:22:29 -0300

    use the proper version for php-parser

--- src/Parser/CustomPrettyPrinter.php
+++ src/Parser/CustomPrettyPrinter.php
@@ -8,20 +8,11 @@ use PhpParser\PrettyPrinter\Standard;
 
 class CustomPrettyPrinter extends Standard
 {
-    public function pExpr_MethodCall(Expr\MethodCall $node)
+    protected function pExpr_MethodCall(Expr\MethodCall $node)
     {
         $call = Grammar::indent('->' . $this->pObjectProperty($node->name)
              . '(' . $this->pMaybeMultiline($node->args) . ')', 1);
 
         return $this->pDereferenceLhs($node->var) . $this->nl . $call;
-    }
-
-    protected function pMaybeMultiline(array $nodes, bool $trailingComma = false)
-    {
-        if (! $this->hasNodeWithComments($nodes)) {
-            return $this->pCommaSeparated($nodes);
-        } else {
-            return $this->pCommaSeparatedMultiline($nodes, $trailingComma) . $this->nl;
-        }
     }
 }
