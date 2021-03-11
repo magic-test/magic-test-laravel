80cbdb86a1a9020072c680c97019279278b55021
Sam Lewis <sam@explicat.io>
2021-03-11 09:29:10 -0500

    Change filter to be an option instead of an argument

--- src/Commands/MagicTestCommand.php
+++ src/Commands/MagicTestCommand.php
@@ -6,14 +6,14 @@ use Illuminate\Console\Command;
 
 class MagicTestCommand extends Command
 {
-    public $signature = 'magic {filter? : Name of specific test to run}';
+    public $signature = 'magic {--filter= : Run a single test}';
 
     public $description = 'Run your Dusk Test Suite using Magic Test.';
 
     public function handle()
     {
         $this->comment('Your Magic Test session is starting...');
-        $filterstring = ($this->argument('filter')) ? ' --filter '.$this->argument('filter') : '';
+        $filterstring = ($this->option('filter')) ? ' --filter '.$this->option('filter') : '';
         shell_exec('DUSK_HEADLESS_DISABLED=1 MAGIC_TEST=1 php artisan dusk '.$filterstring);
     }
 }
