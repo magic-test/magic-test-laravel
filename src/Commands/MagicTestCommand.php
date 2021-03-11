5b5fe12ac4a15eb7214e170f23ae3d4021a4a9ac
Sam Lewis <sam@explicat.io>
2021-03-11 09:21:27 -0500

    Add ability to filter magic by providing test name as argument

--- src/Commands/MagicTestCommand.php
+++ src/Commands/MagicTestCommand.php
@@ -6,14 +6,14 @@ use Illuminate\Console\Command;
 
 class MagicTestCommand extends Command
 {
-    public $signature = 'magic';
+    public $signature = 'magic {filter? : Name of specific test to run}';
 
     public $description = 'Run your Dusk Test Suite using Magic Test.';
 
     public function handle()
     {
         $this->comment('Your Magic Test session is starting...');
-        
-        shell_exec('DUSK_HEADLESS_DISABLED=1 MAGIC_TEST=1 php artisan dusk');
+        $filterstring = ($this->argument('filter')) ? ' --filter '.$this->argument('filter') : '';
+        shell_exec('DUSK_HEADLESS_DISABLED=1 MAGIC_TEST=1 php artisan dusk '.$filterstring);
     }
 }
